fc496039a15ba4ee81636aca544fe429d57f90a5
handsomewang <447843842@qq.com>
2022-06-27 14:23:51 +0800

    项目初始化-引入vant组件库，导入icont图标库

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
-
+    <div class="toutiao toutiao-dianzan"></div>
     <!-- 路由的出口 -->
     <router-view> </router-view>
   </div>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import router from './router/index.js'
 import store from './store/index.js'
 // 加载全局样式
 import './styles/index.less'
+
+// 加载vant组件库
+import Vant from 'vant'
+
+// 加载vant全局样式
+import 'vant/lib/index.css'
+
+// 注册使用vant组件库
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
